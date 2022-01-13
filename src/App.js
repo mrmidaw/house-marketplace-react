de0633424b8ea8ba6b55afc8c4d924bd26656f22
Vadim Gavrilov <mr.midaw@gmail.com>
2022-01-13 15:30:29 +0300

    add listing details

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import { Category } from "./pages/Category";
 import { Navbar } from "./components/Navbar";
 import { PrivateRoute } from "./components/PrivateRoute";
 import { CreateListing } from "./pages/CreateListing";
+import { Listing } from "./pages/Listing";
 
 const App = () => {
   return (
@@ -28,6 +29,7 @@ const App = () => {
           <Route path="/sing-up" element={<SingUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/create-listing" element={<CreateListing />} />
+          <Route path="/category/:categoryName/:listingId" element={<Listing />} />
           <Route path="/*" element={<NotFound />} />
         </Routes>
         <Navbar />
